e7b58b4962f6f6ef109a957b04a3d6b092670ca2
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-11-30 00:30:28 -0300

    Call analysis with general command

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { Probot } from "probot";
+import { exec } from "child_process";
+
+function execute(command: string, callback: Function) {
+  exec(command, function (_error, stdout, _stderr) {
+    callback(stdout);
+  });
+}
 
 export = (app: Probot) => {
   app.on("pull_request.opened", async (context) => {
@@ -6,13 +13,28 @@ export = (app: Probot) => {
     const repo = context.payload.repository.name;
     const prNumber = context.payload.number;
 
-    await context.octokit.pulls.createReview({
-      owner,
-      repo,
-      pull_number: prNumber,
-      body: "Anotação na linha 1!",
-      comments: [{ path: "src/main/java/Text.java", position: 1, body: "Comentário na linha 1!" }],
-      event: "COMMENT"
-    });
+    execute(
+      `D: && cd D:/Arquivos/Documentos/IC/Repositorios/ferramentas de analise/conflict-static-analysis && mvn exec:java -D"exec.mainClass"="br.unb.cic.analysis.Main" -D"exec.args"="-csv target/annotations.csv -cp target/test-classes"`,
+      async (output: string) => {
+        console.log(output);
+        const conflictsLine = output.match(/Number of conflicts:\s*([\d\.]*)\s*\n/);
+        const nConflicts = conflictsLine ? conflictsLine[1] : "[ERROR]";
+        const runtimeLine = output.match(/Total time:\s*([\d\.]*) s/);
+        const runtime = runtimeLine ? runtimeLine[1] : "[ERROR]";
+
+        const commentBody = `# Static Analysis Results
+### Number of conflicts: ${nConflicts}
+### Runtime: ${runtime} s`;
+
+        await context.octokit.pulls.createReview({
+          owner,
+          repo,
+          pull_number: prNumber,
+          body: commentBody,
+          comments: [],
+          event: "COMMENT"
+        });
+      }
+    );
   });
 };
